8ca97d1315c186f24af317e1b1ff7d2763d9ed9d
yaroslav.pylaev <56zeBVPax8kPgjM>
2022-04-02 23:35:04 +0300

    add / listening

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+
+app.get('/', (req, res) => res.send('Webhook worked'));
 app.use('/alice-skill', aliceSkillRouter.router);
+
 app.listen(3000);
 
 module.exports = app;
